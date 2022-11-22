a2a87230aee6aee8bc890407e4f5a904e57cceae
TrueMolni <truemolni@gmail.com>
2022-11-22 17:28:09 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const Refs = {
+  bodyRef: document.querySelector('body'),
+  startButtonRef: document.querySelector('button[data-start]'),
+  stopButtonRef: document.querySelector('button[data-stop]'),
+};
 
+const COLOR = function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+Refs.bodyRef.getAttribute('background-color');
+// Refs.bodyRef.hasAttribute('backgroundColor');
+console.log(Refs.bodyRef.getAttribute('background-color'));
