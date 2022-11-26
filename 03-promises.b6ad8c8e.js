89ddca3f9d4694bba22897d7a7e3e8ebd3476cad
TrueMolni <107940643+TrueMolni@users.noreply.github.com>
2022-11-26 16:13:27 +0000

    Deploying to gh-pages from @ TrueMolni/goit-js-hw-09@7235474f3b96c54cf59b2a1577cba8c37c5a376c 🚀

--- 03-promises.b6ad8c8e.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={delayRef:document.querySelector("input[name='delay']"),stepRef:document.querySelector("input[name='step']"),amountRef:document.querySelector("input[name='amount']"),submitRef:document.querySelector("button")};console.log(e)}();
-//# sourceMappingURL=03-promises.b6ad8c8e.js.map
