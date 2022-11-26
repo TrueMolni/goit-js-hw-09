2259a301440fe91b86ea9852b7a152dda244787a
TrueMolni <truemolni@gmail.com>
2022-11-26 15:50:33 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,62 +1,66 @@
 import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
+// імпорти
 
+// об'єкт налаштувань для flatpickr
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+
+  //метод, де відбувається валідація вибраної дати.
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    const deltaTime = selectedDates[0] - Date.now();
+    if (deltaTime <= 0) {
+      window.alert('Please choose a date in the future');
+    }
+
+    if (deltaTime > 0) {
+      // якщо обрана дата в майбутньому, записуємо цю дату в змінну
+      // робимо кнопку таймеру активною
+      Refs.startButtonRef.removeAttribute('disabled');
+      startTime = selectedDates[0].getTime();
+    }
   },
 };
 
+flatpickr('#datetime-picker', options);
+// глобальні змінні
+let startTime = 0;
+const DELAY = 1000;
+
+// посилання на кнопку, табло таймеру
 const Refs = {
-  inputRef: document.querySelector('#datetime-picker'),
   startButtonRef: document.querySelector('button[data-start]'),
-  timerRef: document.querySelector('.timer'),
   daysValueRef: document.querySelector('span[data-days]'),
   hoursValueRef: document.querySelector('span[data-hours]'),
   minutesValueRef: document.querySelector('span[data-minutes]'),
   secondsValueRef: document.querySelector('span[data-seconds]'),
 };
 
-const fp = flatpickr('#datetime-picker', options);
-
-let id = null;
-const DELAY = 1000;
-// let userTime = [];
-
 Refs.startButtonRef.addEventListener('click', onStartButtonHandle);
-Refs.inputRef.addEventListener('OnClose', OnCloseHandle);
-
+Refs.startButtonRef.setAttribute('disabled', true);
 function onStartButtonHandle() {
   timer();
 }
 
-function OnCloseHandle(event) {
-  userTime = event.target.value;
-  console.log(userTime);
-}
-
+// оновлює значення для табла таймеру
 function render({ days, hours, minutes, seconds }) {
-  Refs.hoursValueRef.textContent = pad(days);
-  Refs.daysValueRef.textContent = pad(hours);
+  Refs.daysValueRef.textContent = pad(days);
+  Refs.hoursValueRef.textContent = pad(hours);
   Refs.minutesValueRef.textContent = pad(minutes);
   Refs.secondsValueRef.textContent = pad(seconds);
 }
-function timer() {
-  const startTime = Date.now();
 
-  id = setInterval(() => {
+// запускаємо зворотній таймер, дату передаємо зі змінної startTime.
+// конвертуємо дату у форматі хх:xx:xx:xx і записуємо дані у табло через рендер
+function timer() {
+  setInterval(() => {
     const currentTime = Date.now();
     const deltaTime = startTime - currentTime;
-    if (deltaTime === 0) {
-      window.alert('Please choose a date in the future');
-      clearInterval(id);
-      return;
-    }
 
     const { days, hours, minutes, seconds } = convertMs(deltaTime);
     console.log(`${pad(days)}:${pad(hours)}:${pad(minutes)}:${pad(seconds)} `);
@@ -65,6 +69,7 @@ function timer() {
   }, DELAY);
 }
 
+// підраховує час, повертає об'єкт з днями, годинами, хвилинами, секундами
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -84,8 +89,7 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
+// передаємо значення, отримуємо рядок для наших спанів у форматі 00
 function pad(value) {
   return String(value).padStart(2, '0');
 }
