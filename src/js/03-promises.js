11a0e9262b3f1c9fee445a8eabc2db63c775981a
TrueMolni <truemolni@gmail.com>
2022-11-27 22:09:29 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,46 +1,24 @@
 import Notiflix from 'notiflix';
 
-function createPromise(position, delay) {
-  let nextDelay = 0;
-  delay = 500;
-  nextDelay += delay;
-  const shouldResolve = Math.random() > 0.3;
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve('success', position);
-      } else {
-        reject('denied', position);
-      }
-    }, nextDelay);
-  });
-}
-
 const Refs = {
   delayRef: document.querySelector("input[name='delay']"),
   stepRef: document.querySelector("input[name='step']"),
   amountRef: document.querySelector("input[name='amount']"),
-  submitRef: document.querySelector('button'),
   formRef: document.querySelector('form'),
 };
-let firstDelay = 0;
-let step = 0;
-let amount = 0;
 
 Refs.formRef.addEventListener('submit', onSubmitHandle);
 Refs.formRef.addEventListener('input', onFormHandle);
 
 function onSubmitHandle(event) {
   event.preventDefault();
-  Refs.stepRef.value = event.target;
-  Refs.amountRef.value = event.target;
-  Refs.delayRef.value = event.target;
-  console.log(Refs.stepRef.value);
-  console.log(Refs.delayRef.value);
+
+  multiple(delay, step, amount);
+  console.log(`${delay}, ${step}, ${amount}`);
 }
 
 // function createMePromise(horse) {
-//   return new Promise(resolve => {
+//   return new Promise((resolve, reject) => {
 //     const time = 1000;
 //     setTimeout(() => {
 //       resolve({ horse, time });
@@ -48,26 +26,43 @@ function onSubmitHandle(event) {
 //   });
 // }
 
-Notiflix.Notify.success(`✅ Fulfilled promise position in delay ms`, {
-  timeout: 2000,
-});
-
-// function createPromise(position, delay) {
-//   let nextDelay = 0;
-//   nextDelay += delay;
-//   const shouldResolve = Math.random() > 0.3;
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         resolve(console.log('yay promise'));
-//       } else {
-//         reject(console.log("can't create promise"));
-//       }
-//     }, nextDelay);
-//   });
-// }
-
 function onFormHandle() {
-  formData.email = refs.inputRef.value;
-  formData.message = refs.textareaRef.value;
+  delay = Refs.delayRef.value;
+  step = Refs.stepRef.value;
+  amount = Refs.amountRef.value;
+}
+
+const multiple = (delay, step, amount) => {
+  for (let i = 0; i < amount; i += 1) {
+    let theDelay = 0;
+    theDelay =
+      Number.parseInt(delay) + Number.parseInt(step) * Number.parseInt(i);
+    position = i + 1;
+
+    console.log(theDelay);
+    createPromise(position, theDelay)
+      .then(({ position, theDelay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${theDelay} ms`
+        );
+      })
+      .catch(({ position, theDelay }) => {
+        Notiflix.Notify.failure(
+          ` ❌ Rejected  promise ${position} in ${theDelay} ms`
+        );
+      });
+  }
+};
+
+function createPromise(position, theDelay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, theDelay });
+      } else {
+        reject({ position, theDelay });
+      }
+    }, theDelay);
+  });
 }
